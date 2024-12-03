bbe4b765fa93437320797100bbe3e5b0bd87d56a
El-kanoun <un.jour.de.printemps1@gmail.com>
2024-12-03 14:54:04 +0100

    v2_dév list AntiHero

--- src/app/anti-hero/pages/list/list.component.ts
+++ src/app/anti-hero/pages/list/list.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AntiHero } from '../../../core/models/anti-hero.interface';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+   // sample data of anti-hero
+   antiHeroes: AntiHero[] = [
+    {
+      firstName: "Eddie",
+      lastName: "Brock",
+      house: "New York",
+      knownAs: "Venom"
+    }
+  ]
+  headers: {headerName: string, fieldName: keyof
+            AntiHero}[] = [
+    {headerName: "First Name", fieldName: "firstName"},
+    {headerName: "Last Name", fieldName: "lastName"},
+    {headerName: "House", fieldName: "house"},
+    {headerName: "Known As", fieldName: "knownAs"},
+  ]
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  selectAntiHero(antiHero: AntiHero){
+    
+  }
 
 }
