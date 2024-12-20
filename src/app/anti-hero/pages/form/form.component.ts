a86b2b7e32878489448cf65e1d305c953338054f
El-kanoun <un.jour.de.printemps1@gmail.com>
2024-12-20 13:00:01 +0100

    v5_dév formulaire reactive dynamique avec FormArray

--- src/app/anti-hero/pages/form/form.component.ts
+++ src/app/anti-hero/pages/form/form.component.ts
@@ -1,28 +1,52 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { checkHasNumberValidator } from '../../../shared/validateurs/checkHasNumberValidator';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
-export class FormComponent implements OnInit {
-  userForm! : FormGroup;
+export class FormComponent {
+  heroForm! : FormGroup;
+  address! : FormGroup;
+  itemForm! : FormGroup;
 
-  constructor(){
+  constructor(private fb: FormBuilder){
+    this.heroForm = this.fb.group({
+      items: this.fb.array([]),
+    });
   }
 
-  ngOnInit(): void {
-    this.userForm = new FormGroup({
-      email: new FormControl(),
-      firstName: new FormControl(),
-      lastName: new FormControl()
+  
+
+  // Getter pour récupérer le FormArray
+  get items(): FormArray {
+    return this.heroForm.get('items') as FormArray;
+  }
+
+  //method  addPower
+  addItem(): void{
+    this.itemForm = this.fb.group({
+      firstName: ['', [checkHasNumberValidator]],
+      lastName: ['', [checkHasNumberValidator]],
+      knownAs:['', [checkHasNumberValidator]],
+      address: this.fb.group({
+        street:[''],
+        city:[''],
+        country:['']
     })
+  })
+  this.items.push(this.itemForm);
+}
+  //method  deletePower
+  removeItem(i: number): void{
+    this.items.removeAt(i);
   }
 
   //method onSubmit(userForm)
-  onSubmit(userForm: any){
-    console.log(userForm.value);
+  onSubmit(heroForm: any){
+    console.log(heroForm.value);
   }
 
  
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -8,6 +8,7 @@ import { MatTableModule } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
 
 
@@ -21,6 +22,7 @@ import { MatInputModule } from '@angular/material/input';
     MatFormFieldModule,
     MatSelectModule,
     MatInputModule,
+    MatCardModule
   ],
 
   exports: [
@@ -32,6 +34,8 @@ import { MatInputModule } from '@angular/material/input';
     MatFormFieldModule,
     MatSelectModule,
     MatInputModule,
+    MatCardModule
+
   ]
 })
 export class MaterialModule { }
